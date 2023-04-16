1b4d1fec7e16c8f8a1db0978a746abbdcd791e33
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2023-04-16 16:02:10 +0900

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# chatgpt-sumally-discord-bot
+
+This Discord bot uses OpenAI's ChatGPT API to retrieve text from URLs included in messages that have been reacted to with the 🔍 emoji, and then responds with a summary of the text.
+
+![Demo](https://user-images.githubusercontent.com/91340399/232278806-e6275b49-329a-4fe7-86eb-8b71beda4c92.png)
+
+## Usage
+
+### environment variables
+
+1. Rename the `.env.example` file and change it to `.env`.
+
+2. Generate an API token from the [OpenAI website](https://platform.openai.com/account/api-keys).
+
+3. Create a bot from [Discord Developer Portal](https://discord.com/developers/applications) and copy the token.
+
+```bash
+OPENAI_API_KEY="for"
+DISOCRD_TOKEN="bar"
+```
+
+### install dependencies
+
+Install dependencies with pip.
+
+```bash
+pip install -r requirements.txt
+```
+
+### run
+
+```bash
+python bot.py
+```
---
+++ bot.py
@@ -0,0 +1,93 @@
+import re
+import discord
+import requests
+import openai
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.reactions = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_raw_reaction_add(payload):
+    text_channel = client.get_channel(payload.channel_id)
+    message = await text_channel.fetch_message(payload.message_id)
+
+    if payload.emoji.name == "🔍":
+        print("Reaction recognized: 🔍")
+        url = extract_url_from_message(message.content)
+        summary, cost = summarize_and_translate(url)
+
+        if summary is None or cost is None:
+            await message.reply("Failed to parse the HTML.")
+        else:
+            print("Replying with summary...")
+            await message.reply(f"{summary}\n\nAPI usage cost: {cost:.4f}$")
+            print("Replied with summary.")
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+def extract_url_from_message(content):
+    url_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    return re.findall(url_pattern, content)[0]
+
+def ask_gpt(prompt):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "入力された文章を要約してください。\n制約条件\n・文章は簡潔にわかりやすく。\n・箇条書きで3行以内で出力。\n・1行あたりの文字数は80文字程度。\n・重要なキーワードは取り逃がさない。\n・要約した文章は日本語へ翻訳。" },
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        message = response["choices"][0]["message"]["content"]
+        tokens_used = int(response["usage"]["total_tokens"])
+        return message, tokens_used
+    except Exception as e:
+        raise Exception(e)
+
+def summarize_and_translate(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    main_content = find_main_content(soup)
+
+    if not main_content:
+        print("🚩 Failed to parse the HTML")
+        return None, None
+
+    content = extract_text_from_content(main_content)
+    if not content:
+        print("🚩 Failed to parse the HTML")
+        return None, None
+    print(f"Content length: {len(content)} characters")
+    summary, tokens_used = ask_gpt(content)
+    cost = tokens_used * 0.000002
+
+    return summary, cost
+
+def find_main_content(soup):
+    for tag in ['article', 'main', 'div']:
+        main_content = soup.find(tag)
+        if main_content:
+            break
+    return main_content
+
+def extract_text_from_content(content):
+    paragraphs = content.find_all('p')
+    if not paragraphs:
+        return None
+    return ' '.join([p.get_text() for p in paragraphs])
+
+client.run(os.getenv("DISOCRD_TOKEN"))
